9e23170b210bf19d1dd9e848f6c6d2ec8344cb8b
Rasmus Jensen <rje@panther.opasia.dk>
2010-12-06 16:09:46 +0100

    wordnet jquerymobile

--- wordnet/simplehtml.py
+++ wordnet/simplehtml.py
@@ -5,11 +5,32 @@ import cgi
 import cgitb
 cgitb.enable()
 
-print 'Content-Type: text/html\n'
-print '<html> <head></head><body>'
+import os
+contentTypes = os.environ["HTTP_ACCEPT"]
+if contentTypes.find("text/html") != -1:
+        print 'Content-Type: text/html'
+elif contentTypes.find("application/xhtml+xml") != -1:
+        print 'Content-Type: application/xhtml+xml'
+else: 
+        print 'Content-Type: application/vnd.wap.xhtml+xml'
+
+
+print """
+<!DOCTYPE html >
+<html>
+<head> 
+        <title>WordNet</title> 
+        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a1/jquery.mobile-1.0a1.min.css" />
+        <script src="http://code.jquery.com/jquery-1.4.3.min.js"></script>
+        <script src="http://code.jquery.com/mobile/1.0a1/jquery.mobile-1.0a1.min.js"></script>
+</head> 
+<body> 
+<div data-role="page"><div data-role="content">
+"""
+
 
 def toLink(word):
-        return '<a href="simplehtml?word=%s">%s</a>' % (word, word.replace("_", " "))
+        return '<a href="/wn/simplehtml?word=%s">%s</a>' % (word, word.replace("_", " "))
 
 def printEntry(entry):
     print "<h1>%s</h1><ul>" % (entry.word.replace("_", " "),)
@@ -35,15 +56,15 @@ def printEntry(entry):
         print "</ul>"
         print "</li>"
     print "</ul>"
-    print '<a href="simplehtml?word=%s+">index</a>' % (entry.word,)
+    print '<a href="/wn/simplehtml?word=%s+" data-role="button" data-inline="true">index</a>' % (entry.word,)
 
 def body():
     params = cgi.FieldStorage()
     word = params.getfirst("word")
     print """
-        <form action="simplehtml" method="GET">
-            <input type="text" name="word" />
-            <input type="submit" value="search"/>
+        <form action="/wn/simplehtml" method="GET">
+                <input type="text" size="20" name="word" />
+                <input type="submit" value="Search" data-inline="true" />
         </form>
         """
     if word == None:
@@ -54,16 +75,22 @@ def body():
         printEntry(entry)
         return
 
-    entries = db.GqlQuery("SELECT * FROM WordNet WHERE word>:1 ORDER BY word ASC", word).fetch(10)
-    entries.extend(db.GqlQuery("SELECT * FROM WordNet WHERE word<:1 ORDER BY word DESC", word).fetch(10))
+    entries = db.GqlQuery("SELECT * FROM WordNet WHERE word>:1 ORDER BY word ASC", word).fetch(5)
+    entries.extend(db.GqlQuery("SELECT * FROM WordNet WHERE word<:1 ORDER BY word DESC", word).fetch(5))
     entries = [str(x.word) for x in entries]
     entries.sort()
-    print "<ul>"
+    print '<div><ul data-role="listview" data-inset="true">'
     for entry in entries:
         print '<li>%s</li>' % (toLink(entry),)
-    print "</ul>"
-    print '<a href="simplehtml?word=%s+">prev</a>' % (entries[0],)
-    print '<a href="simplehtml?word=%s+">next</a>' % (entries[-1],)
+    print "</ul></div>"
+    print '<div data-role="controlgroup" data-type="horizontal">'
+    print '<a data-icon="arrow-l" data-role="button" href="simplehtml?word=%s+">prev</a>' % (entries[0],)
+    print '<a data-icon="arrow-r" data-role="button" href="simplehtml?word=%s+">next</a>' % (entries[-1],)
+    print '</div>'
 
 body()
-print '<div style="text-align:right;font-size:50%"><a href="LICENSE_FOR_DATABASE">database license</a></div></body></html>'
+print """
+<div style="text-align:right;font-size:50%"><a href="http://demo.solsort.dk/wn/LICENSE_FOR_DATABASE">database license</a></div>
+</div></div>
+</body></html>
+"""

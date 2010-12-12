b59e5e1eeb0245e47a48b2b4e80f523fdc07371e
Rasmus Jensen <rje@panther.opasia.dk>
2010-12-12 16:38:45 +0100

    lightscript

---
+++ lightscript/lightscript2/syntax2.js
@@ -0,0 +1,40 @@
+Expressions:
+// ["set" ["id" name] value]
+// ["while" cond body]
+// ["block" ...]
+// ["cond" cond0 body0 cond1 body1 ... body-else]
+// ["comment" ...]
+// ["return" expr]
+// ["and" expr0 expr1]
+// ["or" expr0 expr1]
+// ["call" functionexpr args]
+// ["function" ["args" type0 arg0 type1 arg1 ...] [locals type0 arg0 type1 arg1 ...] body]
+// ["array" expr0 expr1 ...]
+// ["table" key0 val0 key1 val1 ...]
+// ["string" value]
+// ["number" value]
+// ["id" value]
+// ["call" ["id" "[]"] collection key]
+// ["call" ["id" "[]="] collection key value]
+// ["call" ["id" ".()"] collection key value]
+// ["call" ["id" "+"] val0 val1]
+// ["call" ["id" "*"] val0 val1]
+// ["call" ["id" "/"] val0 val1]
+// ["call" ["id" "-"] val0 ?val1?]
+// ["call" ["id" "<"] val0 val1]
+// swap ">"
+// ["call" ["id" "<="] val0 val1]
+// swap ">="
+// ["call" ["id" "==="] val0 val1]
+// ["call" ["id" "!=="] val0 val1]
+// ["call" ["id" "!"] value]
+// ["id" "true"]
+// ["id" "false"]
+// ["id" "undefined"]
+
+// for-in loops
+// (macros)
+// classes
+// catch
+// throw
+// arguments

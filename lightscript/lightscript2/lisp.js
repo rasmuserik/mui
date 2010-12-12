d16e54d17e5c19aa4ffaf0e0051a354c33741984
Rasmus Jensen <rje@panther.opasia.dk>
2010-12-12 21:32:19 +0100

    self-hosted

--- lightscript/lightscript2/lisp.js
+++ lightscript/lightscript2/lisp.js
@@ -1,115 +1,23 @@
+/* Code compiled from yolan */
 load("lisplib.js");
 
-var c = " "
-var getnext = function() {
-    var result = [];
-    while(" \n\r\t".indexOf(c) !== -1) {
-        c = getch();
-    }
-    switch(c) {
-        case "[":
-            c = getch();
-            var elem = getnext();
-            while(elem !== false) {
-                result.push(elem);
-                elem = getnext();
-            }
-            return result;
-            
-        case "]":
-            c = getch();
-            return false;
-        case "'":
-            c = getch();
-            while(c != "'") {
-                if(c == "\\") { 
-                    c = getch(); 
-                    if(c == "n") {
-                        c = "\n";
-                    } else if(c == "r") {
-                        c = "\r";
-                    } else if(c == "t") {
-                        c = "\t";
-                    }
-                }
-                result.push(c);
-                c = getch();
-            }
-            c = getch();
-            return ["str", result.join('')];
-        default:
-            while(" \n\r\t[]'".indexOf(c) == -1) {
-                result.push(c);
-                c = getch()
-            }
-            return result.join("");
-    }
-}
-function getnext(){var result,elem,quote;result=array();while(string_contains(" \n\r\t",c)){nextc()};if((c==="[")){nextc();elem=getnext();while(!((elem===false))){array_push(result,elem);elem=getnext()};return(result)}else if((c==="]")){nextc();return(false)}else if((c==="'")){nextc();while(!((c==="'"))){if((c==="\\")){nextc();if((c==="n")){c="\n"}else if((c==="r")){c="\r"}else if((c==="t")){c="\t"}};array_push(result,c);nextc()};nextc();return(array("str",array_join(result,"")))}else if(string_contains("0123456789",c)){while(string_contains("0123456789",c)){array_push(result,c);nextc()};return(array("num",array_join(result,"")))}else {while(!(string_contains(" \n\r\t[]",c))){array_push(result,c);nextc()};return(array_join(result,""))}}
 
-function tailblock(expr, n) {
-    return "{" + map(compile, tail(expr, n)).join(";") + "}"
-}
 
-function compileif(expr) {
-    var result = "";
-    if(expr[0] != 'else') {
-        result = "if(" + compile(expr[0]) + ")";
-    }
-    result = result + tailblock(expr, 1);
-    return result;
-}
-function infix(opname, expr) {
-    return "(" + compile(expr[1]) + opname + compile(expr[2]) + ")";
-}
+function nextc(){c=getch()}
+nextc()
+function getnext(){var result,elem,quote;result=array();while(string_contains(" \n\r\t",c)){nextc()};if(c==="["){nextc();elem=getnext();while(!(elem===false)){array_push(result,elem);elem=getnext()};return(result)}else if(c==="]"){nextc();return(false)}else if(c==="'"){nextc();while(!(c==="'")){if(c==="\\"){nextc();if(c==="n"){c="\n"}else if(c==="r"){c="\r"}else if(c==="t"){c="\t"}};array_push(result,c);nextc()};nextc();return(array("str",array_join(result,"")))}else if(string_contains("0123456789",c)){while(string_contains("0123456789",c)){array_push(result,c);nextc()};return(array("num",array_join(result,"")))}else {while(!(string_contains(" \n\r\t[]",c))){array_push(result,c);nextc()};return(array_join(result,""))}}
 
-function compile(expr) {
-    var result, i;
-    if(typeof(expr) == "string") {
-        return expr;
-    }
-    switch(expr[0]) {
-        case "define":
-            return "function " + expr[1][0] + "(" 
-                    + tail(expr[1]).join(",") + ")"
-                    + tailblock(expr,2);
-        case "locals":
-            return "var " + tail(expr).join(",");
-        case "while":
-            return "while(" + compile(expr[1]) + ")" 
-                    + tailblock(expr,2);
-        case "switch":
-            return result;
-        case "num":
-            return expr[1];
-        case "str":
-            return uneval(expr[1])
-        case "cond":
-            return map(compileif, tail(expr)).join('else ');
-        case "eq?":
-            return infix('===', expr);
-        case "and":
-            return infix('&&', expr);
-        case "or":
-            return infix('||', expr);
-        case "not":
-            return "!(" + compile(expr[1]) + ")";
-        case "get":
-            return compile(expr[1]) + "[" + compile(expr[2]) + "]";
-        case ";":
-            return "";
-        case "set":
-            return expr[1] + "=" + compile(expr[2]);
-        default:
-            return expr[0] + "(" + map(compile, tail(expr)).join(",") + ")";
-    }
-}
 
-print('load("lisplib.js");');
-var expr = getnext()
-while(expr !== false) {
- // print(uneval(expr));
-//    print(listpp(expr));
-    print(compile(expr));
-    expr = getnext()
-}
+
+function infix(name,expr){return(strjoin(compile(expr[1]),name,compile(expr[2])))}
+function tailblock(expr,n){return(strjoin("{",array_join(map(compile,tail(expr,n)),";"),"}"))}
+function compileif(expr){var condition;if(expr[0]==="else"){condition=""}else {condition=strjoin("if(",compile(expr[0]),")")};return(strjoin(condition,tailblock(expr,1)))}
+function compile(expr){var type;type=expr[0];;if(typeof(expr)==="string"){return(expr)}else if(type==="define"){return(strjoin("function ",expr[1][0],"(",array_join(tail(expr[1]),","),")",tailblock(expr,2)))}else if(type==="locals"){return(strjoin("var ",array_join(tail(expr),",")))}else if(type==="set"){return(infix("=",expr))}else if(type==="num"){return(expr[1])}else if(type==="str"){return(uneval(expr[1]))}else if(type===";"){return("")}else if(type==="get"){return(strjoin(compile(expr[1]),"[",compile(expr[2]),"]"))}else if(type==="not"){return(strjoin("!(",compile(expr[1]),")"))}else if(type==="eq?"){return(infix("===",expr))}else if(type==="cond"){return(array_join(map(compileif,tail(expr)),"else "))}else if(type==="while"){return(strjoin("while(",compile(expr[1]),")",tailblock(expr,2)))}else {return(strjoin(type,"(",map(compile,tail(expr)),")"))}}
+
+
+
+print("/* Code compiled from yolan */")
+print("load(\"lisplib.js\");")
+;
+expr=getnext()
+while(!(expr===false)){;print(compile(expr));expr=getnext()}

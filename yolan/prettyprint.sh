7368b7f20a6c1c69d068456c319078f01cfdaa1b
Rasmus Jensen <rje@panther.opasia.dk>
2010-12-14 00:03:04 +0100

    yolan

--- yolan/prettyprint.sh
+++ yolan/prettyprint.sh
@@ -1,5 +1,5 @@
 cp $1yl _backup_$1yl.`date +%Y%m%d-%H%M%S`
 ./compile.sh prettyprint.
-js prettyprint.js < $1yl > prettyprint_temp.yl &&
-diff $1yl prettyprint_temp.yl &&
-mv prettyprint_temp.yl $1yl
+js prettyprint.js < $1yl > _backup_prettyprint_temp.yl &&
+cp _backup_prettyprint_temp.yl $1yl
+diff $1yl _backup_prettyprint_temp.yl

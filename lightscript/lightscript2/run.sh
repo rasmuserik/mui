5a8be8e916b46e656491c87ab526de527305d8f5
Rasmus Jensen <rje@panther.opasia.dk>
2010-12-19 08:05:11 +0100

    lightscript2: cleanup

--- lightscript/lightscript2/run.sh
+++ lightscript/lightscript2/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#js parser2.js javascript < parser2.js > t.js 
-#js parser2.js yolan < parser2.js > t.yl 
-js parser2.js javascript < parser2.ls > parser2.js.new &&
-    js parser2.js.new lightscript < parser2.ls  > parser2.ls.new && 
-    diff -u parser2.ls parser2.ls.new && 
-    mv parser2.js.new parser2.js && 
-    rm parser2.ls.new
+#js lightscript.js javascript < lightscript.js > t.js 
+#js lightscript.js yolan < lightscript.js > t.yl 
+js lightscript.js javascript < lightscript.ls > lightscript.js.new &&
+    js lightscript.js.new lightscript < lightscript.ls  > lightscript.ls.new && 
+    diff -u lightscript.ls lightscript.ls.new && 
+    mv lightscript.js.new lightscript.js && 
+    rm lightscript.ls.new
 #js parser.js < parser.js
 

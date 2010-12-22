0a9ad1c6c3257aae1c63e401a19438d2e5022d02
Rasmus Jensen <rje@panther.opasia.dk>
2010-12-22 08:21:09 +0100

    lightscript2: bigger test before committing

--- lightscript/lightscript2/commit.sh
+++ lightscript/lightscript2/commit.sh
@@ -1,3 +1,5 @@
-./run.sh && 
-    git add commit.sh lightscript.ls lightscript.js run.sh stdlib.js stdlib.py && 
+./biggerrun.sh && 
+    git diff &&
+    sleep 10 &&
+    git add commit.sh lightscript.ls lightscript.py run.sh stdlib.js stdlib.py biggerrun.sh && 
     git commit -m "lightscript2: $*"
--- lightscript/lightscript2/run.sh
+++ lightscript/lightscript2/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #js lightscript.js javascript < lightscript.js > t.js 
 #js lightscript.js yolan < lightscript.js > t.yl 
-js lightscript.js javascript < lightscript.ls > lightscript.js.new &&
-    js lightscript.js.new lightscript < lightscript.ls  > lightscript.ls.new && 
+python lightscript.py python < lightscript.ls > lightscript.py.new &&
+    python lightscript.py.new lightscript < lightscript.ls  > lightscript.ls.new && 
     diff -u lightscript.ls lightscript.ls.new && 
-    mv lightscript.js.new lightscript.js && 
+    mv lightscript.py.new lightscript.py && 
     rm lightscript.ls.new
 #js parser.js < parser.js
 

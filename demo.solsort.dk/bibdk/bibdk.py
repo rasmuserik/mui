d95e65e61fe3ec2e8ec25c2f3777337da5b53c0e
Rasmus Jensen <rje@panther.opasia.dk>
2010-12-27 18:24:53 +0100

    bibdk

--- demo.solsort.dk/bibdk/bibdk.py
+++ demo.solsort.dk/bibdk/bibdk.py
@@ -2,8 +2,10 @@ import xml.dom.minidom
 import urllib
 import urllib2
 import cgi
+import os
 
 def bibdkRequest(request, startRecord = "1", maximumRecords = "10"):
+    """Send a request to bibliotek.dk, and transform the result into json"""
     query = {}
     query['version'] = "1.1"
     query['operation'] = "searchRetrieve"
@@ -26,16 +28,16 @@ def bibdkRequest(request, startRecord = "1", maximumRecords = "10"):
             nodename = node.nodeName
             if nodename is not '#text' and node.firstChild is not None:
                 arr = record.get(nodename, [])
-                arr.append(node.firstChild.data)
+                arr.append(node.firstChild.data.replace("&", "&amp;"))
                 record[nodename] = arr
         records.append(record)
         recno = recno + 1
     return {"hits": hits, "records": records, "query": request, "startRecord": startRecord}
 
+# these are fields for requests
 fields = ["dc.creator", "dc.title", "dc.subject", "dc.identifier", "cql.serverChoice", "dc.identifier", "dc.date", "dc.type", "dc.language", "bath.personalName", "bath.possessingInstitution", "rec.id", "bath.notes"]
 
-button = lambda title, action : (["button", title, action])
-
+# Utility functions for buiding ui tree
 def title(txt): return ["title", txt]
 def textinput(title, id): return ["textinput", title, id]
 def button(title): return ["button", title]
@@ -44,7 +46,34 @@ def form(url, content):
     result.extend(content)
     return result
 
+def toXHTML(ui):
+    """Transform a ui tree into jquery-mobile-html-code"""
+    if type(ui) == str:
+        return ui
+    elif type(ui) == unicode:
+        return ui
+    elif ui[0] is "form":
+        return u'<form action="%s" method="get">%s</form>' % (ui[1], u''.join(map(toXHTML, ui[2:])))
+    elif ui[0] is "textinput":
+        return u'<div><label for="%s">%s</label> <input type="text" inputmode="latin predictOff" name="%s" id="%s" value="" /></div>' % (ui[2], toXHTML(ui[1]), ui[2], ui[2])
+    elif ui[0] is "button":
+        return u'<div><input type="submit" value="%s" name="submit" /></div>' % (ui[1],)
+    elif ui[0] is "links":
+        return u'<div>%s<ul>%s</ul></div>' % (ui[1], u"".join([
+            u'<li><a href="%s">%s</a><br/>%s</li>' % (e[0], toXHTML(e[1]), toXHTML(e[2])) for e in ui[2]]))
+    elif ui[0] is "text":
+        return u"".join(map(toXHTML, ui[1:]))
+    elif ui[0] is "p":
+        return "<p>" + u"".join(map(toXHTML, ui[1:])) + "</p>"
+    elif ui[0] is "small":
+        return "<small>" + u"".join(map(toXHTML, ui[1:])) + "</small>"
+    elif ui[0] is "menu":
+        return '<div>' + " ".join(['<a href="%s">%s</a>' % (e[0], toXHTML(e[1])) for e in ui[1:]]) + '</div>'
+    else:
+        return u"unknown ui: " + unicode(ui[0])
+
 def toHTML(ui):
+    """Transform a ui tree into jquery-mobile-html-code"""
     if type(ui) == str:
         return ui
     elif type(ui) == unicode:
@@ -57,42 +86,29 @@ def toHTML(ui):
         return u'<div><input type="submit" value="%s" name="submit" /></div>' % (ui[1],)
     elif ui[0] is "links":
         return u'<ul data-role="listview" data-inset="true" data-dividertheme="a"><li data-role="list-divider">%s</li>%s</ul>' % (ui[1], u"".join([
-            u'<li><h3><a href="%s">%s</a></h3><p>%s</p></li>' % (e[0], toHTML(e[1]), toHTML(e[2])) for e in ui[2:]]))
+            u'<li><h3><a href="%s">%s</a></h3><p>%s</p></li>' % (e[0], toHTML(e[1]), toHTML(e[2])) for e in ui[2]]))
     elif ui[0] is "text":
         return u"".join(map(toHTML, ui[1:]))
+    elif ui[0] is "p":
+        return "<p>" + u"".join(map(toXHTML, ui[1:])) + "</p>"
     elif ui[0] is "small":
         return "<small>" + u"".join(map(toHTML, ui[1:])) + "</small>"
     elif ui[0] is "menu":
         return '<div data-role="controlgroup" data-type="horizontal">' + " ".join(['<a data-role="button" href="%s">%s</a>' % (e[0], toHTML(e[1])) for e in ui[1:]]) + '</div>'
     else:
-        return u"unknown ui: " + ui[0]
+        return u"unknown ui: " + str(ui[0])
 
 searchform = form("/bib", [
     textinput("Forfatter:", "dc.creator"), 
     textinput("Titel:", "dc.title"), 
     textinput("Emne:", "dc.subject"), 
     textinput("Fritekst:", "cql.serverChoice"), 
-    button("S&oslash;g")])
+    button("S&#248;g")])
 
-htmlheader = """Content-Type: text/html; charset=UTF-8
-
-<!DOCTYPE html PUBLIC "-//OMA//DTD XHTML Mobile 1.2//EN"
-   "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd"> 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"> 
-<head> 
-        <title>solsort.dk/bibliotek...</title> 
-        <link rel="stylesheet" href="/static/jquery.mobile.min.css" /> 
-        <script src="http://code.jquery.com/jquery-1.4.4.min.js" type="text/ecmascript"></script> 
-        <script src="/static/jquery.mobile.min.js" type="text/ecmascript"></script> 
-</head> 
-<body>
-<div data-role="page" data-theme="b">
-        <div data-role="content">"""
-htmlfooter = """
-        </div>
-</div>
-</body>
-</html>"""
+xhtmlheader = '<!DOCTYPE html PUBLIC "-//OMA//DTD XHTML Mobile 1.2//EN" "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><head><title>solsort.dk/bibliotek...</title></head><body>'
+xhtmlfooter = '</body></html>'
+htmlheader = '<!DOCTYPE html><html><head><title>solsort.dk/bibliotek...</title><link rel="stylesheet" href="/static/jquery.mobile.min.css" /><script src="http://code.jquery.com/jquery-1.4.4.min.js" type="text/ecmascript"></script><script src="/static/jquery.mobile.min.js" type="text/ecmascript"></script></head><body><div data-role="page" data-theme="b"><div data-role="content">'
+htmlfooter = '</div></div></body></html>'
 
 def cons(a, b):
     result = [a]
@@ -103,40 +119,98 @@ def push(list, val):
     list.append(val)
     return list
 
+def text(*args):
+    return cons("text", args)
+
+def links(*args):
+    return cons("links", args)
+
 def printrecords(result):
     hits = int(result["hits"]) 
     startRecord = int(result['startRecord'])
     endRecord = startRecord + len(result['records']) - 1
+    if startRecord > endRecord:
+        startRecord = endRecord
 
-    return push(["text", 'S&oslash;gning: "', result["query"], '"', cons("links", cons('Viser post %d-%d ud af %d' % (startRecord, endRecord, hits), [[
-        record['dc:identifier'][0], 
-        push(cons("text", record.get('dc:title', ['Uden titel'])), cons("small", " (" + ", ".join(record.get('dc:date', [''])) + ") ")),
-        u" & ".join(record.get('dc:creator', ['N.N.']))]
-            for record in result["records"]]))],
-            ["menu", ["prev", "Forrige"], ["next", "N&aelig;ste"], ["/bib", "Ny s&oslash;gning"]])
+    def shortRecordFormat(record):
+        uri = record['dc:identifier'][0]
+        uri = "/bib?" + urllib.urlencode({"query": result["query"], "startRecord": record["recno"], "singleRecord": True}).replace("&", "&amp;")
+        title = ["text", record.get('dc:title', ['Uden titel'])[0], ["small", " (" + ", ".join(record.get('dc:date', [''])) + ") "]]
+        subtitle = u" &amp; ".join(record.get('dc:creator', ['N.N.']))
+        return [uri, title, subtitle]
 
+    menu = ["menu", ["javascript:history.back(-1)", "Tilbage"]]
+    if endRecord < hits:
+        menu.append(["/bib?" + urllib.urlencode({"query": result["query"], "startRecord": endRecord + 1}).replace("&", "&amp;"), "N&#230;ste"])
+    menu.append(["/bib", "Ny s&#248;gning"])
+
+    return text(["p",'S&#248;gning: "', result["query"], '"'],
+            links('Viser post %d-%d ud af %d' % (startRecord, endRecord, hits), map(shortRecordFormat, result["records"])),
+            menu)
+
+def printrecord(result):
+    acc = cons("text", [["p", key, ": ", ", ".join(result["records"][0][key])] for key in result["records"][0]]);
+    menu = ["menu"]
+    menu.append(["javascript:history.back(-1)", "Tilbage"])
+    menu.append(["javascript:alert('Not implemented yet')", "Bestil"])
+    menu.append(["/bib", "Ny s&#248;gning"])
+    return text(acc, menu)
+
+
+smartphone = False
 def printheader():
-    print htmlheader
+    global smartphone
+    if "WebKit" in os.environ.get("HTTP_USER_AGENT", ""):
+        smartphone = True
+
+    print "Content-Type: ",
+    if not smartphone:
+        accept = os.environ.get("HTTP_ACCEPT", "")
+        if "application/vnd.wap.xhtml+xml" in accept:
+            print "application/vnd.wap.xhtml+xml; charset=UTF-8\n"
+        elif "application/xhtml+xml" in accept:
+            print "application/xhtml+xml; charset=UTF-8\n"
+        else:
+            print "text/html; charset=UTF-8\n"
+        print xhtmlheader
+    else:
+        print "text/html; charset=UTF-8\n"
+        print htmlheader
 
 def printbody(body):
-    print toHTML(body).encode('ascii', 'xmlcharrefreplace')
+    #print body
+    if smartphone:
+        print toHTML(body).encode('ascii', 'xmlcharrefreplace')
+    else:
+        print toXHTML(body).encode('ascii', 'xmlcharrefreplace')
 
 def printfooter():
-    print htmlfooter
+    if smartphone:
+        print htmlfooter
+    else:
+        print xhtmlfooter
 
 def main():
     printheader()
-    params = cgi.FieldStorage()
+    form = cgi.FieldStorage()
+    params = {}
+    for key in form:
+        params[key] = form.getvalue(key)
+
     search = []
     for param in params:
         if param in fields:
-            search.append("%s = (%s)" % (param, params.getvalue(param)))
+            search.append("%s = (%s)" % (param, params.get(param)))
     search = " and ".join(search)
-    if not len(search) is 0:
-        result = bibdkRequest(search)
-    if len(search) is 0 or int(result["hits"]) is 0:
+    search = search or params.get("query", "")
+
+    if len(search) is 0:
         printbody(searchform)
+    elif params.get("singleRecord", False):
+        result = bibdkRequest(search, params.get("startRecord", "1"), 1)
+        printbody(printrecord(result))
     else:
+        result = bibdkRequest(search, params.get("startRecord", "1"))
         printbody(printrecords(result))
 
     printfooter()

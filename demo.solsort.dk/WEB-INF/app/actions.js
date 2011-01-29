32b626e8762cd4e9401189e86d1a73ce2a60ccb6
Rasmus Jensen <rje@panther.opasia.dk>
2011-01-29 15:48:48 +0100

    cleanup

--- demo.solsort.dk/WEB-INF/app/actions.js
+++
@@ -1,9 +0,0 @@
-foo = require('ringo/httpclient');
-
-exports.index = function (req) {
-    return {
-        status: 200,
-        headers: {"Content-Type": "text/html"},
-        body: ["<html><head><title></title></head><body>", uneval(req), uneval(foo) ,"</body></html>"]
-    };
-};

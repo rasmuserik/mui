2b0a9526b408c502c93282e1ed3c76bda6b5eedb
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2011-03-24 14:27:28 +0100

    module system

--- js/muiApp.js
+++ js/muiApp.js
@@ -299,7 +299,8 @@ if (!Object.create) {
     }
 
     
-    window.onload=muiMain;
+    // FIXME
     setTimeout(muiMain, 500);
+
     document.write('<div id="container"><div id="current"></div><div id="prev"></div><div id="loading">loading...</div></div>');
 });

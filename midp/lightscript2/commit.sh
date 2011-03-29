873518efc12c9b2418a0228ef06f5dedb3c299f7
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2011-03-29 11:04:22 +0200

    midp cleanup, and removed wtk dependency

--- midp/lightscript2/commit.sh
+++
@@ -1,6 +0,0 @@
-git diff &&
-./biggerrun.sh && 
-    git diff &&
-    sleep 10 &&
-    git add commit.sh lightscript.ls lightscript.py run.sh stdlib.js stdlib.py biggerrun.sh && 
-    git commit -m "lightscript2: $*"

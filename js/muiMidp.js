cafbb4076ec171deb2934035933c22aaeaae8d3e
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2011-04-01 19:12:10 +0200

    midp

--- js/muiMidp.js
+++ js/muiMidp.js
@@ -14,8 +14,50 @@ require("xmodule").def("muiMidp", function() {
     console.log("D");
     mui.loading();
 
+    function childReduce(arr, fn, acc) {
+        if(!Array.isArray(arr)) {
+            console.log("Error: childReduce expected array, got:", arr);
+        }
+        var first = arr[1];
+        if(typeof(first) === "object" && Array.isArray(first)) {
+            acc = fn(acc, first);
+        }
+        for(var i=2; i < arr.length; ++i) {
+            acc = fn(acc, arr[i]);
+        }
+        return acc;
+    }
+    var formelem;
+    function showSubPage(acc, p) {
+        if(Array.isArray(p)) {
+            var type = p[0];
+            if(type == "section") {
+                childReduce(p, showSubPage);
+                console.log(type);
+            } else if(type == "input") {
+                types = {
+                    textbox: { type: 0, len: 5000},
+                    email: {type: 1, len: 20},
+                    tel: { type: 3, len: 20}
+                    };
+                type = types[p[1].type];
+                formelem[p[1].name] = textfield(p[1].label || "", type.len, type.type);
+            } else if(type == "text") {
+                stringitem(p[1]);
+            } else if(type == "button") {
+                addbutton(p[2], p[1].fn);
+            } else {
+                console.log("Unexpected page element:", type);
+            }
+        } else {
+            console.log("Unexpected page element:", p);
+        }
+    }
+
     mui.showPage = function(page) {
+        formelem = {};
         newForm(page[1].title || "untitled");
+        childReduce(page, showSubPage);
     }
 
     console.log("E");
